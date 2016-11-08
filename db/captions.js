69ae9cfc42437899645999773d695f53bc4a81fc
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-11-08 13:43:14 +0000

    Checkpoint: create captions interface

--- db/captions.js
+++ db/captions.js
@@ -4,4 +4,60 @@ AWS.config.update({
   region:      'us-west-2',
 });
 
-const client = new AWS.DynamoDB.DocumentClient();
+const client    = new AWS.DynamoDB.DocumentClient();
+const tableName = 'captionsStaging';
+
+module.exports = {
+  create: create,
+
+  forSubmission: forSubmission,
+
+  get: get,
+
+  addLike: addLike,
+
+  addHate: addHate,
+}
+
+function forSubmission(submissionId, cb) {
+  client.query({
+    TableName: tableName,
+    IndexName: 'submissionId-score',
+    KeyConditionExpression: 'submissionId = :submissionId',
+    ScanIndexForward: false,
+    Limit: 1000,
+    ExpressionAttributeValues: {
+      ':submissionId': submissionId,
+    }
+  }, function(err, payload) {
+    if( err ) { return cb(err); }
+    return cb(null, payload.Items);
+  });
+}
+
+function create(caption, cb) {
+  client.put({
+    TableName: tableName,
+    Item: caption
+  }, cb);
+}
+
+function get(id, cb) {
+  client.get({
+    TableName: tableName,
+    Key: {
+      id: id
+    }
+  }, function(err, payload) {
+    if( err ) { return cb(err); }
+    return cb(null, payload.Item);
+  });
+}
+
+function addLike(id) {
+
+}
+
+function addHate(id) {
+
+}
